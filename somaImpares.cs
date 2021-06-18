9f16555b25485f7550ae5da3a229b88e2b4f45c3
Sh4z4m <86075435+D4rks31d@users.noreply.github.com>
2021-06-18 15:33:25 -0300

    Add files via upload

---
+++ somaImpares.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace somaImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, sumeven, troca, i;
+
+            Console.WriteLine("Digite dois números:");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                troca = x;
+                x = y;
+                y = troca;
+            }
+
+            sumeven = 0;
+
+            for (i = x+1; i < y; i++)
+            {
+                if (i%2 != 0)
+                {
+                    sumeven = sumeven + i;
+                }
+            }
+
+            Console.WriteLine("Soma dos ímpares = " + sumeven);
+        }
+    }
+}
