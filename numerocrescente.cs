9212d46ffc92842725ac8ca54cfd7eb860671629
Sh4z4m <86075435+D4rks31d@users.noreply.github.com>
2021-06-18 14:43:30 -0300

    Add files via upload

---
+++ numerocrescente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace crescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            x = 1;
+            y = 0;
+
+            while (x != y)
+            {
+                Console.WriteLine("Digite dois números: ");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+                
+                if (x > y)
+                {
+                    Console.WriteLine("Decrescente!");
+                    Console.WriteLine();
+                }
+                else if (x < y)
+                {
+                    Console.WriteLine("Crescente!");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
