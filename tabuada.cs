144d29fee3cca91b6b9c5141de64fce5786ee61e
Sh4z4m <86075435+D4rks31d@users.noreply.github.com>
2021-06-18 15:18:47 -0300

    Add files via upload

---
+++ tabuada.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, i;
+
+            Console.Write("Deseja saber a tabuada para qual valor? ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(x + " x " + i + " = " + x * i);
+            }
+        }
+    }
+}
