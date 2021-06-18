0134b33538fc5ce41c02d67946361e67c52c3e33
Sh4z4m <86075435+D4rks31d@users.noreply.github.com>
2021-06-18 15:08:00 -0300

    Add files via upload

---
+++ paresConsecutivos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace paresConsecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, sumodd;
+
+            x = 1;
+
+            while (x != 0)
+            {
+                Console.Write("Digite um número inteiro: ");
+                x = int.Parse(Console.ReadLine());
+
+                sumodd = 0;
+
+                if (x % 2 == 0)
+                {
+                    sumodd = (x) + (x + 2) + (x + 4) + (x + 6) + (x + 8);
+                }
+                else
+                {
+                    sumodd = (x) + (x + 2) + (x + 4) + (x + 6) + (x + 8) + 5;
+                }
+
+                Console.WriteLine("Soma = " + sumodd);
+                Console.WriteLine();
+            }
+        }
+    }
+}
