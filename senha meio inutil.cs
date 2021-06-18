0015042b08f347eaad26b890def31ff71c95b3d2
Sh4z4m <86075435+D4rks31d@users.noreply.github.com>
2021-06-18 15:13:04 -0300

    Add files via upload

---
+++ senha meio inutil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            Console.Write("Digite a senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.Write("Tente novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido!");
+        }
+    }
+}
