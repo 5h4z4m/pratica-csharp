27b18297c17ee9cbbfc6a03c1849cce7746adb75
Sh4z4m <86075435+D4rks31d@users.noreply.github.com>
2021-06-18 14:55:40 -0300

    Add files via upload

---
+++ mediaIdades.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace mediaIdades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, somaidade, pessoas;
+            double media;
+
+            Console.WriteLine("Digite as idades: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade < 0)
+            {
+                Console.WriteLine("Impossível calcular");
+            }
+            else
+            {
+                somaidade = 0;
+                pessoas = 0;
+
+                while (idade >= 0)
+                {
+                    somaidade = somaidade + idade;
+                    pessoas = pessoas + 1;
+
+                    idade = int.Parse(Console.ReadLine());
+                }
+
+                media = (double)somaidade / pessoas;
+
+                Console.WriteLine("Media = " + media.ToString("F2"));
+            }
+        }
+    }
+}
